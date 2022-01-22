07eaf63c3847e02aa46e802079366806d4c11c5e
ilionic <ilionic@gmail.com>
2022-01-22 15:00:43 +0100

    update deps, use upstream uniques

--- pallets/rmrk-core/src/lib.rs
+++ pallets/rmrk-core/src/lib.rs
@@ -283,7 +283,7 @@ pub mod pallet {
 				sender.clone(),
 				T::ClassDeposit::get(),
 				false,
-				pallet_uniques::Event::Created(collection_id, sender.clone(), sender.clone()),
+				pallet_uniques::Event::Created { class: collection_id, creator: sender.clone(), owner: sender.clone() },
 			)?;
 
 			Self::deposit_event(Event::CollectionCreated { issuer: sender, collection_id });
