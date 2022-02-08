858259b2dee321dd395f85bfb7ffc477baf753ca
bmacer <bmacer@cisco.com>
2022-02-08 17:17:27 -0500

    WIP tests

--- pallets/rmrk-equip/src/tests.rs
+++ pallets/rmrk-equip/src/tests.rs
@@ -29,12 +29,14 @@ macro_rules! bvec {
 fn create_base_works() {
 	ExtBuilder::default().build().execute_with(|| {
 		let fixed_part = FixedPart {
-			id: stb("fixed_part_id"),
+			// id: stb("fixed_part_id"),
+			id: 100,
 			z: 0,
 			src: stb("fixed_part_src"),
 		};
 		let slot_part = SlotPart {
-			id: stb("slot_part_id"),
+			// id: stb("slot_part_id"),
+			id: 102,
 			z: 0,
 			src: stb("slot_part_src"),
 			equippable: vec![
@@ -62,12 +64,12 @@ fn create_base_works() {
 fn equip_works() {
 	ExtBuilder::default().build().execute_with(|| {
 		let fixed_part = FixedPart {
-			id: stb("fixed_part_id"),
+			id: 111,
 			z: 0,
 			src: stb("fixed_part_src"),
 		};
 		let slot_part = SlotPart {
-			id: stb("slot_part_id"),
+			id: 222,
 			z: 0,
 			src: stb("slot_part_src"),
 			equippable: vec![
@@ -124,19 +126,19 @@ fn equip_works() {
 		));
 
 		let fixed_part = FixedPart {
-			id: stb("fixed_part_id"),
+			id: 333,
 			z: 0,
 			src: stb("fixed_part_src"),
 		};
 
 		let fixed_part_2 = FixedPart {
-			id: stb("fixed_part_id2"),
+			id: 444,
 			z: 0,
 			src: stb("fixed_part_src"),
 		};
 
 		let slot_part = SlotPart {
-			id: stb("slot_part_id"),
+			id: 555,
 			z: 0,
 			src: stb("slot_part_src"),
 			equippable: vec![
@@ -157,38 +159,11 @@ fn equip_works() {
 				],
 		);
 
-		// println!("iter");
-		// println!("pp: {:?}", <Parts<Test>>::iter_prefix_values(0).count());
-		// println!("pp: {:?}", <Parts<Test>>::iter_prefix_values(0).collect::<NewPartTypes<StringLimitOf<Test>>>());
-		for i in <Parts<Test>>::iter_prefix_values(0) {
-			println!("i: {:?}", i);	
-		}
-		// let x: NewPartTypes<StringLimitOf<Test>> = <Parts<Test>>::iter_prefix_values(0).collect();
-		// println!("end iter");
-
-		// println!("parts: \n{:?}", RmrkEquip::parts(0, 2));
-		// println!("end parts");
-
 		assert_ok!(RmrkCore::new_add_resource(
 			Origin::signed(ALICE),
-			0,
-			0,
-			ResourceType::Base(
-				NoncomposableResource { //<BaseId, SlotId, ResourceId, BoundedString> {
-					base: 0, // pub base: BaseId,
-					slot_id: 0, // pub slot_id: SlotId,
-					id: 0, // pub id: ResourceId,
-					src: stb("src"), // pub src: BoundedString,
-					thumb: None, // pub thumb: Option<BoundedString>,
-					theme_id: None, // pub themeId: Option<BoundedString>,
-				}
-			), // slot
-		));
-
-		assert_ok!(RmrkCore::new_add_resource(
-			Origin::signed(ALICE),
-			0,
-			0,
+			0, // collection_id
+			0, // nft id
+			Some(0), // base id
 			ResourceType::Slot(
 				ComposableResource { //<BaseId, SlotId, ResourceId, BoundedString> {
 					base: 0, // pub base: BaseId,
@@ -203,6 +178,57 @@ fn equip_works() {
 			), // slot
 		));
 
+		assert_ok!(RmrkCore::new_add_resource(
+			Origin::signed(ALICE),
+			0, // collection id
+			0, // nft id
+			None, // base id
+			ResourceType::Base(
+				NoncomposableResource { //<BaseId, SlotId, ResourceId, BoundedString> {
+					base: 0, // pub base: BaseId,
+					slot_id: 0, // pub slot_id: SlotId,
+					id: 0, // pub id: ResourceId,
+					src: stb("src"), // pub src: BoundedString,
+					thumb: None, // pub thumb: Option<BoundedString>,
+					theme_id: None, // pub themeId: Option<BoundedString>,
+				}
+			), // slot
+		));
+
+		assert_ok!(RmrkCore::new_add_resource(
+			Origin::signed(ALICE),
+			0, // collection id
+			0, // nft id
+			None, // base id
+			ResourceType::Base(
+				NoncomposableResource { //<BaseId, SlotId, ResourceId, BoundedString> {
+					base: 0, // pub base: BaseId,
+					slot_id: 0, // pub slot_id: SlotId,
+					id: 0, // pub id: ResourceId,
+					src: stb("src"), // pub src: BoundedString,
+					thumb: None, // pub thumb: Option<BoundedString>,
+					theme_id: None, // pub themeId: Option<BoundedString>,
+				}
+			), // slot
+		));
+
+		assert_ok!(RmrkCore::new_add_resource(
+			Origin::signed(ALICE),
+			1, // collection id
+			0, // nft id
+			None, // base id
+			ResourceType::Base(
+				NoncomposableResource { //<BaseId, SlotId, ResourceId, BoundedString> {
+					base: 0, // pub base: BaseId,
+					slot_id: 1, // pub slot_id: SlotId,
+					id: 100, // pub id: ResourceId,
+					src: stb("src"), // pub src: BoundedString,
+					thumb: None, // pub thumb: Option<BoundedString>,
+					theme_id: None, // pub themeId: Option<BoundedString>,
+				}
+			), // slot
+		));
+
 		// println!("New res: {:?}", RmrkCore::new_resources((0,0,1)).unwrap());
 
 		// (src_col, src_nft), (dest_col, dest_nft), base_id, part_id
@@ -212,101 +238,26 @@ fn equip_works() {
 			0, // Item NftId
 			0, // Equipper CollectionId
 			0, // Equipper NftId
-			1, // BaseId
-			3, // SlotId
+			0, // BaseId
+			1, // SlotId
 		));
 
-		// Does item exist?
-		// Does equipper nft exist?
-		// Does caller own item?
-		// Does caller own equipper?
-		// Does base.slot exist?
-		// Is item collection in base.slot's equippable list?
-
-
-		/*
-pub struct ComposableResource<BaseId, ResourceId, BoundedString> {
-	pub base: BaseId,
-	pub id: ResourceId,
-	pub parts: Vec<BoundedString>, // maybe switch to Vec<Part> ?
-	pub src: Option<BoundedString>,
-	pub thumb: Option<BoundedString>,
-}
-		*/
-
-		// println!("CORE:\n {:?}", RmrkCore::new_resources((0,0,1)));
-
-
-
-
-
-		// Separately, a BASE may be defined of a wide array of parts of two categories, fixed and slot.
-		// - Fixed parts are *defined* pieces.  For example, hairstyle-bald, hairstyle-curly, with specific data defining what these
-		// pieces are.
-		// - Slot parts are *placeholder* pieces.  They don't define what will take its spot, rather they define only which
-		// collection IDs qualify for filling this slot.
-
-		// Once a BASE is defined, the issuer of a collection may RESADD a resource to an NFT in that collection.  This would define
-		// the combination of fixed and slot parts that compose this resource.
-
-		// Once the resource has been ACCEPTed by the NFTs owner, equipping operations may begin.
-
-		// If an NFT owns an NFT that is a member of a collection that is EQUIPPABLE into a base's slot, it may be EQUIPped
-
-		// Should not be able to equip onto non-existing NFT
-		// Should not be able to equip onto non-owned NFT
-		// Should not be able to equip a non-existent base
-
-		// RmrkEquip::equip();
-
-		/*
-
-		Here's a RESADD for a kanaria:
-		resadd: ["RMRK", 
-		"RESADD", 
-		"2.0.0",
-		"8949171-e0b9bdcc456a36497a-KANBIRD-KANL-00009479", 
-		"%7B%22base%22%3A%22base-8788686-KANBASE%22%2C%22id%22%3A%22ugYtHT0b%22%2C%22parts%22%3A%5B%22var4_beak%22%2C%22
-		var2_body%22%2C%221f32b_eyes%22%2C%22var2_footLeft%22%2C%22var2_footRight%22%2C%22var2_handLeft%22%2C%222694-fe0f_handright%22
-		%2C%22var2_head%22%2C%22var2_tail%22%2C%22var2_wingLeft%22%2C%222694-fe0f_wingright%22%2C%22card_le%22%2C%22background%22%2C
-		%22foreground%22%2C%22headwear%22%2C%22backpack%22%2C%22objectleft%22%2C%22objectright%22%2C%22necklace%22%2C%22gem_empty2%22
-		%2C%22gem_empty3%22%2C%22gem_empty4%22%5D%2C%22thumb%22%3A%22ipfs%3A%2F%2Fipfs%2FQmR3rK1P4n24PPqvfjGYNXWixPJpyBKTV6rYzAS2TYHLpT
-		%22%2C%22themeId%22%3A%22mattel%22%7D"]
-
-		Decoded:
-		{
-			"base":"base-8788686-KANBASE",
-			"id":"6BrBxg_X",
-			"parts":["1F60F_beak","var4_body","1F60F_eyes","var4_footLeft","var4_footRight",
-				"var4_handLeft","1f48e_handRight","1f48e_head","var4_tail","var4_wingLeft","1f48e_wingRight","card_le","background",
-				"foreground", "headwear","backpack","objectleft","objectright","necklace","gem_empty2","gem_empty3","gem_empty4"],
-			"thumb":"ipfs://ipfs/QmR3rK1P4n24PPqvfjGYNXWixPJpyBKTV6rYzAS2TYHLpT",
-			"themeId":"eggplant"
+		for i in <pallet_rmrk_core::NewResources<Test>>::iter_prefix_values((0, 0, None::<BaseId>)) {
+			println!("iii: {:?}", i);
 		}
 
-		RESADD for new years eve sparkler:
-		"RMRK", 
-		"RESADD",
-		"2.0.0",
-		"10758408-e0b9bdcc456a36497a-EVNTS-NYE22-00000193",
-		"%7B%22slot%22%3A%22base-8788686-KANBASE.objectright%22%2C%22
-		id%22%3A%22fG-kSh3M%22%2C%22src%22%3A%22ipfs%3A%2F%2Fipfs%2FQmPAdcr87vu2mENZbJNg14yoRRjtUZ6vAYWN5oZQczyCvo%2F
-		sparklers22_objectright.svg%22%2C%22thumb%22%3A%22ipfs%3A%2F%2Fipfs%2FQmPAdcr87vu2mENZbJNg14yoRRjtUZ6vAYWN5oZQczyCvo%2F
-		sparklers22_object_thumb.png%22%7D"
+		println!("aaa: {:?}", <pallet_rmrk_core::NewResources<Test>>::get((0, 0, None::<BaseId>, 1)));
+		println!("aaa: {:?}", <pallet_rmrk_core::NewResources<Test>>::get((0, 0, None::<BaseId>, 2)));
 
-		Decoded:
-		{
-			"slot":"base-8788686-KANBASE.objectright",
-			"id":"fG-kSh3M",
-			"src":"ipfs://ipfs/QmPAdcr87vu2mENZbJNg14yoRRjtUZ6vAYWN5oZQczyCvo/sparklers22_objectright.svg",
-			"thumb":"ipfs://ipfs/QmPAdcr87vu2mENZbJNg14yoRRjtUZ6vAYWN5oZQczyCvo/sparklers22_object_thumb.png"
+		for i in <pallet_rmrk_core::NewResources<Test>>::iter_prefix_values((0, 0, Some(0))) {
+			println!("jjj: {:?}", i);
 		}
 
-		Create TWO resadd operations.  (1) BaseResAdd and (2) SlotResAdd.  
+		for i in Equippings::<Test>::iter_prefix_values(((0, 0), 0)) {
+			println!("kkk: {:?}", i);
+		}
 
 
-		*/
 
-		// println!("{:?}", RmrkEquip::bases(0).unwrap());
 	});
 }
