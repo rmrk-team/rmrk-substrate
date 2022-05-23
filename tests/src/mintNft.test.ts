24eab1a890aee464637dc31660b64b69c57af756
Daniel Shiposha <dshiposha@usetech.com>
2022-05-23 13:53:13 +0000

    remove: unneeded comment about mintNft

--- tests/src/mintNft.test.ts
+++ tests/src/mintNft.test.ts
@@ -37,16 +37,6 @@ describe("Integration test: mint new NFT", () => {
     });
 
     it("mint NFT and set another owner", async () => {
-        // NOTE: This test won't pass on our fork
-        // but it will pass on the original rmrk-substrate.
-        // The RMRK team have fixed the issue with `uniques` owner.
-        //
-        // - See the difference -
-        //     Our fork: https://github.com/UniqueNetwork/rmrk-substrate/blob/3a1e5cc3e9b9da0a01b5667916b646e62e5147a2/pallets/rmrk-core/src/lib.rs#L297-L302
-        // The original: https://github.com/rmrk-team/rmrk-substrate/blob/27f9e27b784d223b9e4ec05dc9817293c6b8e4d6/pallets/rmrk-core/src/lib.rs#L305-L310
-        //
-        // This test was run on the original and successfully passed.
-
         const owner = bob;
         const collectionMetadata = 'setOwnerCollectionMetadata';
         const collectionMax = null;
