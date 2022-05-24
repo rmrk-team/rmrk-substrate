59b41f092fe37953dc5d087b3c13136c877eef2f
Daniel Shiposha <dshiposha@usetech.com>
2022-05-24 12:07:57 +0000

    test: Burn nft with children

--- tests/src/burnNft.test.ts
+++ tests/src/burnNft.test.ts
@@ -1,6 +1,13 @@
 import { getApiConnection } from "./substrate/substrate-api";
 import { expectTxFailure } from "./util/helpers";
-import { burnNft, createCollection, lockCollection, mintNft } from "./util/tx";
+import { NftIdTuple, getChildren } from './util/fetch';
+import { burnNft, createCollection, sendNft, mintNft } from "./util/tx";
+
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
 
 describe("Integration test: burn nft", () => {
   const Alice = "//Alice";
@@ -26,8 +33,55 @@ describe("Integration test: burn nft", () => {
         collectionId,
         "nft-metadata"
       );
-      await burnNft(api, nftId, collectionId, Alice);
+      await burnNft(api, Alice, collectionId, nftId);
     });
+  });
+
+  it("Burn nft with children", async () => {
+    const collectionId = await createCollection(
+      api,
+      Alice,
+      "test-metadata",
+      null,
+      "test-symbol"
+    );
+
+    const parentNftId = await mintNft(
+      api,
+      Alice,
+      Alice,
+      collectionId,
+      "nft-metadata"
+    );
+
+    const childNftId = await mintNft(
+      api,
+      Alice,
+      Alice,
+      collectionId,
+      "nft-metadata"
+    );
+
+    const newOwnerNFT: NftIdTuple = [collectionId, parentNftId];
+
+    await sendNft(api, "sent", Alice, collectionId, childNftId, newOwnerNFT);
+
+    const childrenBefore = await getChildren(api, collectionId, parentNftId);
+    expect(childrenBefore.length === 1, 'Error: parent NFT should have children')
+      .to.be.true;
+
+    let child = childrenBefore[0];
+    expect(child.collectionId.eq(collectionId), 'Error: invalid child collection Id')
+      .to.be.true;
+
+    expect(child.nftId.eq(childNftId), 'Error: invalid child NFT Id')
+      .to.be.true;
+
+    await burnNft(api, Alice, collectionId, parentNftId);
+
+    const childrenAfter = await getChildren(api, collectionId, parentNftId);
+
+    expect(childrenAfter.length === 0, 'Error: children should be burned').to.be.true;
   });
 
   it("[Negative] Burn non-existing NFT", async () => {
@@ -38,7 +92,7 @@ describe("Integration test: burn nft", () => {
       null,
       "test-symbol"
     ).then(async (collectionId) => {
-      const tx = burnNft(api, 99999, collectionId, Alice);
+      const tx = burnNft(api, Alice, collectionId, 99999);
       await expectTxFailure(/rmrkCore.NoAvailableNftId/, tx);
     });
   });
@@ -58,7 +112,7 @@ describe("Integration test: burn nft", () => {
         collectionId,
         "nft-metadata"
       );
-      const tx = burnNft(api, nftId, collectionId, Bob);
+      const tx = burnNft(api, Bob, collectionId, nftId);
       await expectTxFailure(/rmrkCore.NoPermission/, tx);
     });
   });
