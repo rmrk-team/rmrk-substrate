b628e12a97263319bb8acc06502fad819ae7a372
Sergej Sakac <sakacszergej@gmail.com>
2022-11-16 08:00:06 +0100

    less repetition

--- pallets/rmrk-market/src/benchmarking.rs
+++ pallets/rmrk-market/src/benchmarking.rs
@@ -55,6 +55,10 @@ fn create_test_collection<T: Config>(
 	let metadata = bvec![0u8; 20];
 	let max = None;
 	let symbol = bvec![0u8; 15];
+	<T as pallet_uniques::Config>::Currency::make_free_balance_be(
+		&caller,
+		50_000_000u64.saturated_into(),
+	);
 	let _ = RmrkCore::<T>::create_collection(
 		(RawOrigin::Signed(caller.clone())).into(),
 		collection_id.clone(),
@@ -129,7 +133,6 @@ benchmarks! {
 	list {
 		let caller: T::AccountId = whitelisted_caller();
 		let collection_index = 1;
-		<T as pallet_uniques::Config>::Currency::make_free_balance_be(&caller, BalanceOf::<T>::max_value());
 
 		let collection_id = create_test_collection::<T>(caller.clone(), collection_index);
 		let nft_id = mint_test_nft::<T>(caller.clone(), None, collection_id, 42);
@@ -142,7 +145,6 @@ benchmarks! {
 	unlist {
 		let caller: T::AccountId = whitelisted_caller();
 		let collection_index = 1;
-		<T as pallet_uniques::Config>::Currency::make_free_balance_be(&caller, BalanceOf::<T>::max_value());
 
 		let collection_id = create_test_collection::<T>(caller.clone(), collection_index);
 		let nft_id = mint_test_nft::<T>(caller.clone(), None, collection_id, 42);
@@ -185,9 +187,6 @@ benchmarks! {
 	accept_offer {
 		let caller: T::AccountId = whitelisted_caller();
 		let collection_index = 1;
-		// The balance is not set to `max_value` because the caller needs to
-		// accept `MinimumOfferAmount::get()` without an overflow.
-		<T as pallet_uniques::Config>::Currency::make_free_balance_be(&caller, 50_000_000u64.saturated_into());
 
 		let collection_id = create_test_collection::<T>(caller.clone(), collection_index);
 		let nft_id = mint_test_nft::<T>(caller.clone(), None, collection_id, 42);
